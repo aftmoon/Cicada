c99fbfd1703efcf5d9f6c209c5074ab63ef7baea
cc <ttangling0@gmail.com>
2025-03-30 21:41:36 +0800

    feat: 三连击动画切换逻辑 + 基础闪避特效

---
+++ Assets/CCC/MHYTest/StrikeJaegerResources/Jaeger_Attrat.cs
@@ -0,0 +1,204 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class Jaeger_Attrat : MonoBehaviour
+{
+    public Animator animator;
+    public ActionState currentState;
+    //移动相关
+    public IdleState idleState;
+    public NormalAttact normalAttact;
+    public NormalAttact02 normalAttact02;
+    public SpecialAttact specialAttact;
+    //闪避相关
+    public Dodge dodge;
+    public GameObject dodgePrefab;
+
+    //判定
+    internal bool leftMouse_KeyDown;
+    internal bool leftControl_KeyDown;
+    internal bool Q_KeyDown;
+    public AnimatorStateInfo info;
+    public string stateName;
+    public void IsTouched()
+    {
+        Debug.Log("Touched!!!!!!cccccccsc");
+    }
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        idleState = new IdleState() { playerC = this };
+        normalAttact = new NormalAttact() { playerC = this }; ;
+        normalAttact02 = new NormalAttact02() { playerC = this }; ;
+        specialAttact = new SpecialAttact() { playerC = this }; ;
+        dodge = new Dodge() { playerC = this }; ;
+        currentState = idleState;
+        currentState.Enter();
+
+    }
+
+    void Update()
+    {
+        leftMouse_KeyDown = Input.GetMouseButtonDown(0);
+        //TODO!  ccc左边ctrl键坏了哈哈
+        leftControl_KeyDown = Input.GetKeyDown(KeyCode.RightControl);
+        Q_KeyDown = Input.GetKeyDown(KeyCode.Q);
+
+        //leftControl_KeyDown = Input.GetKeyDown(KeyCode.LeftControl);
+        bool input_attact = leftControl_KeyDown | leftMouse_KeyDown | Q_KeyDown;
+        info = this.animator.GetCurrentAnimatorStateInfo(0);
+        //if(currentState != idleState && !animator.IsInTransition(0) && info.IsName("Move"))
+            bool ifa = info.IsName("Move");
+        Debug.Log("currentState != idleState IS " + ifa);
+        if((currentState.GetType().ToString() != "idleState") && !animator.IsInTransition(0) && info.IsName("Move"))
+        {
+            Debug.Log("change idle");
+            idleState.Enter();
+
+        }
+        if (input_attact)
+        {
+            currentState.HandleInput();
+        }
+    }
+
+    
+    public bool CanAttact(float start, float end)
+    {
+        return info.normalizedTime >= start && info.normalizedTime <= end;
+    }
+    
+    //闪避判定
+    public bool CanDodge(float start, float end)
+    {
+        return true;
+        //return info.normalizedTime >= start && info.normalizedTime <= end;
+    }
+    
+}
+public class Dodge : ActionState
+{
+
+    public override void Enter()
+    {
+        Debug.Log("Dodge!!!!!cccc");
+        base.Enter();
+        playerC.animator.CrossFade("Dodge", 0.1f);
+        GameObject dodgePrefab = playerC.dodgePrefab.gameObject;
+        Transform spawnPoint = playerC.transform;
+        GameObject dodgeClone = Instantiate(dodgePrefab, spawnPoint.position, spawnPoint.rotation);
+
+        Animator dodgeAnimator = dodgeClone.GetComponent<Animator>();
+        if (dodgeAnimator != null)
+        {
+            dodgeAnimator.Play("Dodge", 0, playerC.animator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+        }
+
+        // **等待动画播放完毕后销毁**
+        float dodgeAnimLength = playerC.animator.GetCurrentAnimatorStateInfo(0).length;
+        Destroy(dodgeClone, dodgeAnimLength);
+
+    }
+    public override void HandleInput()
+    {
+        base.HandleInput();
+        if (playerC.Q_KeyDown)
+            playerC.dodge.Enter();
+    }
+
+}
+
+public class SpecialAttact : ActionState
+{
+    public override void Enter()
+    {
+        base.Enter();
+        playerC.animator.CrossFade("Attact04", 0.1f);
+    }
+
+}
+
+public class NormalAttact : ActionState
+{
+    public override void Enter()
+    {
+        base.Enter();
+        playerC.animator.CrossFade("Attact01", 0.1f);
+    }
+    public override void HandleInput()
+    {
+        if (playerC.CanAttact(0.6f, 0.9f))
+        {
+            base.HandleInput();
+            if (playerC.leftMouse_KeyDown)
+                playerC.normalAttact02.Enter();
+            else if (playerC.leftControl_KeyDown)
+                playerC.specialAttact.Enter();
+
+        }
+    }
+}
+
+public class NormalAttact02 : ActionState
+{
+    public override void Enter()
+    {
+        base.Enter();
+        playerC.animator.CrossFade("Attact01_2", 0.1f);
+    }
+    public override void HandleInput()
+    {
+        base.HandleInput();
+        if (playerC.CanAttact(0.7f, 0.9f))
+        {
+            if (playerC.leftControl_KeyDown)
+                playerC.specialAttact.Enter();
+        }
+
+    }
+}
+
+
+public class IdleState: ActionState
+{
+    public override void Enter()
+    {
+        base.Enter();
+        playerC.animator.CrossFade("Move", 0.01f);
+    }
+    public override void HandleInput()
+    {
+        base.HandleInput();
+        if (playerC.leftMouse_KeyDown)
+            playerC.normalAttact.Enter();
+        else if (playerC.leftControl_KeyDown)
+            playerC.specialAttact.Enter();
+        if (playerC.Q_KeyDown && playerC.CanDodge(0,0))
+        {
+            playerC.dodge.Enter();
+        }
+    }
+}
+
+
+public class ActionState:MonoBehaviour
+{
+    public Jaeger_Attrat playerC;
+    public virtual void Enter()
+    {
+        OnEnter();
+    }
+    protected void OnEnter()
+    {
+        this.playerC.currentState = this;
+        this.playerC.stateName = this.GetType().ToString();
+    }
+    public virtual void HandleInput()
+    {
+    }
+   
+}
--- Assets/Script/UI/UIManager.cs
+++ Assets/Script/UI/UIManager.cs
@@ -1,40 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class UIManager : MonoBehaviour
-{
-    public GameObject uiPrefab; // 你的 UI 预制体
-    public int uiCount = 5; // 生成的 UI 数量
-    public float verticalSpacing = 100f; // UI 元素之间的垂直间距
+//public class UIManager : MonoBehaviour
+//{
+//    public GameObject uiPrefab; // 你的 UI 预制体
+//    public int uiCount = 5; // 生成的 UI 数量
+//    public float verticalSpacing = 100f; // UI 元素之间的垂直间距
 
-    private Canvas canvas;
+//    private Canvas canvas;
 
-    void Start()
-    {
-        canvas = FindObjectOfType<Canvas>();
-        if (canvas == null)
-        {
-            Debug.LogError("No Canvas found in the scene.");
-            return;
-        }
+//    void Start()
+//    {
+//        canvas = FindObjectOfType<Canvas>();
+//        if (canvas == null)
+//        {
+//            Debug.LogError("No Canvas found in the scene.");
+//            return;
+//        }
 
-        // 生成 UI 元素
-        for (int i = 0; i < uiCount; i++)
-        {
-            // 创建 UI 元素并设置为 Canvas 的子对象
-            GameObject uiElement = Instantiate(uiPrefab, canvas.transform);
-            RectTransform rectTransform = uiElement.GetComponent<RectTransform>();
+//        // 生成 UI 元素
+//        for (int i = 0; i < uiCount; i++)
+//        {
+//            // 创建 UI 元素并设置为 Canvas 的子对象
+//            GameObject uiElement = Instantiate(uiPrefab, canvas.transform);
+//            RectTransform rectTransform = uiElement.GetComponent<RectTransform>();
 
-            // 设置位置
-            rectTransform.anchoredPosition = new Vector2(0, -i * verticalSpacing);
-            //Debug.Log($"Generated UI Element at position: {rectTransform.anchoredPosition}"); // 打印生成位置
+//            // 设置位置
+//            rectTransform.anchoredPosition = new Vector2(0, -i * verticalSpacing);
+//            //Debug.Log($"Generated UI Element at position: {rectTransform.anchoredPosition}"); // 打印生成位置
 
-            // 添加 UI 拖拽功能
-            uiElement.AddComponent<DragUI>();
+//            // 添加 UI 拖拽功能
+//            uiElement.AddComponent<DragUI>();
 
-            // 添加 UI 排斥功能
-            uiElement.AddComponent<RepelUI>();
-        }
-    }
-}
+//            // 添加 UI 排斥功能
+//            uiElement.AddComponent<RepelUI>();
+//        }
+//    }
+//}
