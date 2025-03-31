508b23efe2f4e9235ee43f447f7cfdb9255287e0
cc <ttangling0@gmail.com>
2025-03-31 22:22:16 +0800

    fix : 操作键位对应

--- Assets/CCC/MHYTest/StrikeJaegerResources/Jaeger_Attrat.cs
+++ Assets/CCC/MHYTest/StrikeJaegerResources/Jaeger_Attrat.cs
@@ -19,8 +19,8 @@ public class Jaeger_Attrat : MonoBehaviour
 
     //判定
     internal bool leftMouse_KeyDown;
-    internal bool leftControl_KeyDown;
-    internal bool Q_KeyDown;
+    internal bool rightMouse_KeyDown;
+    internal bool Space_KeyDown;
     public AnimatorStateInfo info;
     public string stateName;
     public void IsTouched()
@@ -45,11 +45,11 @@ public class Jaeger_Attrat : MonoBehaviour
     {
         leftMouse_KeyDown = Input.GetMouseButtonDown(0);
         //TODO!  ccc左边ctrl键坏了哈哈
-        leftControl_KeyDown = Input.GetKeyDown(KeyCode.RightControl);
-        Q_KeyDown = Input.GetKeyDown(KeyCode.Q);
+        rightMouse_KeyDown = Input.GetMouseButtonDown(1);
+        Space_KeyDown = Input.GetKeyDown(KeyCode.Space);
 
         //leftControl_KeyDown = Input.GetKeyDown(KeyCode.LeftControl);
-        bool input_attact = leftControl_KeyDown | leftMouse_KeyDown | Q_KeyDown;
+        bool input_attact = rightMouse_KeyDown | leftMouse_KeyDown | Space_KeyDown;
         info = this.animator.GetCurrentAnimatorStateInfo(0);
         //if(currentState != idleState && !animator.IsInTransition(0) && info.IsName("Move"))
             bool ifa = info.IsName("Move");
@@ -85,7 +85,6 @@ public class Dodge : ActionState
 
     public override void Enter()
     {
-        Debug.Log("Dodge!!!!!cccc");
         base.Enter();
         playerC.animator.CrossFade("Dodge", 0.1f);
         GameObject dodgePrefab = playerC.dodgePrefab.gameObject;
@@ -95,7 +94,7 @@ public class Dodge : ActionState
         Animator dodgeAnimator = dodgeClone.GetComponent<Animator>();
         if (dodgeAnimator != null)
         {
-            dodgeAnimator.Play("Dodge", 0, playerC.animator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+            //dodgeAnimator.Play("Dodge", 0, playerC.animator.GetCurrentAnimatorStateInfo(0).normalizedTime);
         }
 
         // **等待动画播放完毕后销毁**
@@ -106,7 +105,7 @@ public class Dodge : ActionState
     public override void HandleInput()
     {
         base.HandleInput();
-        if (playerC.Q_KeyDown)
+        if (playerC.Space_KeyDown)
             playerC.dodge.Enter();
     }
 
@@ -136,7 +135,7 @@ public class NormalAttact : ActionState
             base.HandleInput();
             if (playerC.leftMouse_KeyDown)
                 playerC.normalAttact02.Enter();
-            else if (playerC.leftControl_KeyDown)
+            else if (playerC.rightMouse_KeyDown)
                 playerC.specialAttact.Enter();
 
         }
@@ -155,7 +154,7 @@ public class NormalAttact02 : ActionState
         base.HandleInput();
         if (playerC.CanAttact(0.7f, 0.9f))
         {
-            if (playerC.leftControl_KeyDown)
+            if (playerC.rightMouse_KeyDown)
                 playerC.specialAttact.Enter();
         }
 
@@ -175,9 +174,9 @@ public class IdleState: ActionState
         base.HandleInput();
         if (playerC.leftMouse_KeyDown)
             playerC.normalAttact.Enter();
-        else if (playerC.leftControl_KeyDown)
+        else if (playerC.rightMouse_KeyDown)
             playerC.specialAttact.Enter();
-        if (playerC.Q_KeyDown && playerC.CanDodge(0,0))
+        if (playerC.Space_KeyDown && playerC.CanDodge(0,0))
         {
             playerC.dodge.Enter();
         }
